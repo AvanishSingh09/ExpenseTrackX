3d0936e32f8bf89212799e7352c224208c8d0b95
AVANISH SINGH <singhavanish8930@gmail.com>
2026-09-23 22:37:07 +0530

    frontend url updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8081/api',
+    baseURL: 'https://expensetrackx.onrender.com/api',
 });
 
 // Add a request interceptor to include the JWT token
